7814608270b876c04bbc6cd4c0745278f8ae6378
Natalie Bastos <nsb29@njit.edu>
2018-12-17 08:45:11 -0500

    updated armor values

--- g_items.c
+++ g_items.c
@@ -18,9 +18,9 @@ void Weapon_Railgun (edict_t *ent);
 void Weapon_BFG (edict_t *ent);
 void Weapon_Null(edict_t *ent);
 
-gitem_armor_t jacketarmor_info	= { 25,  50, .30, .00, ARMOR_JACKET};
-gitem_armor_t combatarmor_info	= { 50, 100, .60, .30, ARMOR_COMBAT};
-gitem_armor_t bodyarmor_info	= {100, 200, .80, .60, ARMOR_BODY};
+gitem_armor_t jacketarmor_info	= { 0,  50, .30, .00, ARMOR_JACKET}; //changed armor values all to 0, no armor for them!
+gitem_armor_t combatarmor_info	= { 0, 100, .60, .30, ARMOR_COMBAT};
+gitem_armor_t bodyarmor_info	= {0, 200, .80, .60, ARMOR_BODY};
 
 static int	jacket_armor_index;
 static int	combat_armor_index;
